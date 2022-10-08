acc861e49b0abe10189f4e97a2fa43064c2983da
Atirsaw <awokeeyrw@gmail.com>
2022-10-08 11:33:52 +0300

    Adding Array

--- DayFour.java
+++ DayFour.java
@@ -2,5 +2,9 @@ public class DayFour
 {
     public static void main(String[] args) {
         System.out.println("This is for day four");
+
+        int[] arra= {1,2,5,6};
+        System.out.println(arra.length);
+
     }
 }
